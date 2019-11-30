8abea14ad68307fd9c0eaeedaf3f94a48872b118
naveen <naveensingh.mpt@gmail.com>
2019-11-30 17:47:40 +0530

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaincontentComponent } from './maincontent/maincontent.component';
+import {MatTabsModule} from '@angular/material/tabs';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MaincontentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatCardModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300
+    })
+ 
+     
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/maincontent/maincontent.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+
+
+@Component({
+  selector: 'app-maincontent',
+  templateUrl: './maincontent.component.html',
+  styleUrls: ['./maincontent.component.css']
+})
+export class MaincontentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
